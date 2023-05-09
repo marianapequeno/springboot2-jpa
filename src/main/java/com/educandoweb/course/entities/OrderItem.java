4abb5ecc2b52aeeb5ba5efd47f558ac62829cb7d
marianapequeno <marianapqueno@gmail.com>
2023-05-09 15:50:48 -0300

    OrderItem, manyToMany assoc with extra attribut 2

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.entities;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -16,7 +17,7 @@ public class OrderItem implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @EmbeddedId
-  private OrderItemPK id;
+  private OrderItemPK id = new OrderItemPK();
   private Integer quantity;
   private Double price;
 
@@ -29,6 +30,7 @@ public class OrderItem implements Serializable {
     this.price = price;
   }
 
+  @JsonIgnore
   public Order getOrder() {
     return id.getOrder();
   }
